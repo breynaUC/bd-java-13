90d70cdde311c3df22f2530107e2e8d2a47600e5
Docente <Docente@UCATEPALC103_1>
2025-06-23 11:42:04 -0500

    v2.2

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,15 +1,23 @@
 
 package com.mycompany.mavenproject1;
+import com.google.gson.Gson;
 import com.mycompany.config.Conexion;
+import com.mycompany.dao.ProductoDao;
+import com.mycompany.daoImpl.PrductoDaoImpl;
 
 /**
  *
  * @author Docente
  */
 public class Mavenproject1 {
-
+static ProductoDao dao = new PrductoDaoImpl();
+static Gson gson = new Gson();
     public static void main(String[] args) throws ClassNotFoundException {
-       if(Conexion.getConnection()!=null){
+        System.out.println(gson.toJson(dao.readAll()));
+    }
+    
+    static void conexion(){
+        if(Conexion.getConnection()!=null){
            System.out.println("SI");
        }else{
            System.out.println("NO");
